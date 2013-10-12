237c9ffbd50a08ff772a44017fa1125a2cac6232
Kovalyov <kovalyov@kovalyov.(none)>
2013-10-12 15:58:48 +0300

    add devise auth

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Comment do
   it { should belong_to :post}
   it { should validate_presence_of :body}
+
+  it { should have_db_column(:body).of_type(:text) }
+  it { should have_db_column(:post_id).of_type(:integer) }
+
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,10 @@ describe Post do
   it { should have_many :comments }
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
+
+
+  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:body).of_type(:text) }
+
+
 end
